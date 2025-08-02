91a8cf035582a046dcedadc83fed6b6222cf0240
kaushalsingh0410 <kaushalsingh8013@gmail.com>
2025-08-02 12:05:49 +0530

    Fix makemigrations

---
+++ mrs/app/migrations/__init__.py
@@ -0,0 +1,3 @@
+# This file is required to make the migrations directory a Python package.
+# Even if it's empty, do NOT delete it.
+# Migration files are intentionally excluded from version control.
